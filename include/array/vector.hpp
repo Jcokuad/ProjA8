8bee92a2c746d70a8f21fd989d375d382a1c948a
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2026-03-23 14:11:45 -0400

    Updated files with A7 implementation

--- include/array/vector.hpp
+++ include/array/vector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <algorithm>     // provides max
 #include <stdexcept>     // provides out_of_range exception
+#include <utility>
 
 namespace dsac::array {
 
@@ -11,31 +12,135 @@ class Vector {
         int sz{0};
         T* data{nullptr};
     public:
-        Vector();
-        int capacity();
-        int size();
-        bool empty() const;
+        //empty
+        Vector() : cap(0), sz(0), data(nullptr) {}   // initially no memory allocated
         
-        const T& operator[](int i) const;
-        T& operator[](int i);
+        //capacity
+        int capacity() const {
+            return cap;
+        }
+
+        //how many elements stored
+        int size() const {
+            return sz;
+        }
         
-        const T& at(int i) const;
-        T& at(int i);
+        // True is empty
+        bool empty() const {
+            return sz == 0;
+        }
+        
+        //element at index when vector is const
+        const T& operator[](int i) const {
+            return data[i];
+        }
+        
+        //element at index when vector is non-const
+        T& operator[](int i) {
+            return data[i];
+        }
+        
+        // at function for const
+        const T& at(int i) const {
+            if (i < 0 || i >= sz)
+                throw std::out_of_range("Invalid Index");
+            return data[i];
+        }
+        
+        // at function for non const
+        T& at(int i) {
+            if (i < 0 || i >= sz)
+                throw std::out_of_range("Invalid Index");
+            return data[i];
+        }
+        
+        // first element
+        const T& front() const {
+            return data[0];   //first index
+        }
 
-        const T& front() const;
-        T& front();
+        // first element
+        T& front() {
+            return data[0];
+        }
+        
+        // last element
+        const T& back() const {
+            return data[sz - 1];  //last index
+        }
 
-        const T& back() const;
-        T& back();
+        // last element
+        T& back() {
+            return data[sz - 1];
+        }
+        
+        // insert at end
+        void push_back(const T& elem) {  
+            
+            if (sz == cap) { // if the # of elements in the array is at capacity
+                reserve(std::max(1, 2*cap)); //allocate double the current capacity if the array is not zero. If zero, increase by 1.
+            }
 
-        void push_back(const T& elem);
+            data[sz++] = elem; //input element at the first free index
+            // sz++; increment the # of elements in array
+        }
 
-        void pop_back();
+        // remove from end
+        // also call shrink
+        void pop_back() {
+            if (sz == 0) {
+                return;
+            }
 
-        void insert(int i, const T& elem);
-        void erase(int i);
+            sz--; //reduces how much of the array is considered active
+            shrink(); // checks if capacity needs to be updated
+        }
 
-        void reserve(int minimum);
+        // insert at index
+        void insert(int i, const T& elem) {
+            if (i < 0 || i > sz) { // ensure index is within bounds
+                throw std::out_of_range("Invalid Index");
+            }
+
+            if (sz == cap) { // check if capacity needs to be doubled
+                reserve (std::max(1, 2*cap));
+            }
+
+            for (int j = sz - 1; j >= i; --j) {  
+                data[j + 1] = std::move(data[j]); //move each element up by 1 index, from the index of the last element to the index of the insert
+            }
+            data[i] = elem; //insert the element in the index i
+            sz++; // increment the # of elements in the array
+        }
+
+        // removes at index
+        // also call shrink
+        void erase(int i) {
+            if (i < 0 || i >= sz) { // ensure index is within bounds
+                throw std::out_of_range("Invalid Index");
+            }
+
+            for (int j = i + 1; j < sz; j++) {  
+                data[j - 1] = std::move(data[j]); //move each element down by 1 index, from the index of the last element to the index of the insert
+            }
+            sz--; // decrement the # of elements in the array
+            shrink(); // checks if capacity needs to be updated
+        }
+
+        //capacity >= minimum
+        void reserve(int minimum) {
+            if (cap >= minimum) { //there is enough capacity, so no need to allocate more space
+                return;
+            }
+
+            T* temp = new T[minimum]; //creates a temporary array with the min amt of space needed for the next operation
+            for (int i = 0; i < sz; i++) {
+                temp[i] = std::move(data[i]); //moves the values from data to temp then deallocates memory from data
+            }
+            delete[] data; //free the memory from the old data array
+            data = temp; //update the data with new memory
+            cap = minimum; //update capacity with new amt of memory allocated in updated data array
+        }
 
         Vector(const Vector& other);
 
@@ -96,9 +201,43 @@ class Vector {
 
         iterator erase(iterator it);
 
-        void shrink();
+        // called by other functions to reduce cap by half 
+        // when sz <= cap/4 
+        void shrink() {
+
+            if (cap <= 1) {
+                return;
+            }
+
+            if (sz <= cap/4) {
+                int lessCap = cap / 2; // reduces cap by half when sz is a fourth of the current cap
+
+                if (lessCap < 1) {
+                    lessCap = 1;
+                }
+
+                T* temp = new T[lessCap]; //creates a temporary array with half the cap of the current array
+                for (int i = 0; i < sz; i++) {
+                    temp[i] = std::move(data[i]); //moves the values from data to temp then deallocates memory from data
+                }
+                delete[] data; //free the memory from the old data array
+                data = temp; //update the data with new memory
+                cap = lessCap; //update capacity with new amt of memory allocated in updated data array
+            }
+        }
         
-        void shrink_to_fit();
+        // explicitly reduce the cap to sz and keep at least 1 slot
+        void shrink_to_fit() {
+            int lessCap = std::max(1, sz); // selects the current # of elements unless the sz is 0, then leaves 1 slot
+
+            T* temp = new T[lessCap]; //creates a temp array of sz + 1
+            for (int i = 0; i < sz; i++) {
+                temp[i] = std::move(data[i]); // place current elements in temp array and deallocate from data
+            }
+            delete[] data; //free the memory from the old data array
+            data = temp; //update data array with explicitly sized temp array
+            cap = lessCap; //update data array capacity with new amt of memory allocated in temp array
+        }
     
 }; // Vector class
 
--- tests/test_vector_1.cpp
+++ tests/test_vector_1.cpp
@@ -1,13 +1,174 @@
 // test_vector.cpp
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
-
+#include <stdexcept> //for out_of_range exception
 #include "array/vector.hpp"
 
+using dsac::array::Vector; // using statement allows me to use Vector instead of writing the full namespace from the header file
 
 TEST_CASE("Default constructor creates empty vector", "[tag]") {
     dsac::array::Vector<int> v;
     bool ok = (v.size() == 0) && (v.capacity() == 0) && v.empty();
 
     REQUIRE(ok);
+}
+
+TEST_CASE("Check capacity, size, and empty with default no values") {
+    Vector<int> v;
+
+    REQUIRE(v.capacity() == 0);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.empty() == true);
+}
+
+TEST_CASE("Check operator[] and at() for const and non-const") {
+    Vector<int> v;
+
+    for (int i{0}; i < 5; ++i) 
+        v.push_back(i); // 0, 1, 2, 3, 4
+    
+    //check non-const operator[]
+    REQUIRE(v[0] == 0);
+    REQUIRE(v[4] == 4);
+
+    //check non-const at()
+    v[1] = 30;
+    REQUIRE(v.at(1) == 30);
+
+    //check const operator[]
+    const Vector<int>& cv = v;
+    REQUIRE(cv[3] == 3);
+    REQUIRE(cv[1] == 30);
+}
+
+TEST_CASE("Check out of bounds throws for at()") {
+    Vector<int> v;
+
+    REQUIRE_THROWS_AS(v.at(6), std::out_of_range);
+    REQUIRE_THROWS_AS(v.at(-3), std::out_of_range);
+}
+
+TEST_CASE("Check front() and back() for const and non-const") {
+    Vector<int> v;
+
+    for (int i{0}; i < 5; ++i) 
+        v.push_back(i * 3); // 0, 3, 6, 9, 12
+
+    REQUIRE(v.front() == 0);
+    REQUIRE(v.back() == 12);
+
+    v.push_back(15);
+    REQUIRE(v.front() == 0);
+    REQUIRE(v.back() == 15);
+}
+
+TEST_CASE("Check push_back() that cap is never less than sz and correct order") {
+    Vector<int> v;
+
+    for (int i = 0; i < 15; i++) {
+       v.push_back(i);
+       REQUIRE(v.size() == i + 1);
+       REQUIRE(v.capacity() >= v.size()); // ensure capacity stays larger than # of elements
+    }
+}
+
+TEST_CASE("Check pop_back() that size decreases but stops on empty vector") {
+    Vector<int> v;
+
+    //check empty
+    v.pop_back();
+    REQUIRE(v.empty());
+    REQUIRE(v.size() == 0);
+
+    //check with values
+    for (int i{0}; i < 5; ++i) 
+        v.push_back(i * 3); // 0, 3, 6, 9, 12
+
+    v.pop_back();
+    REQUIRE(v.size() == 4);
+    REQUIRE(v.back() == 9);
+
+    v.pop_back();
+    v.pop_back();
+    REQUIRE(v.size() == 2);
+    REQUIRE(v.back() == 3);
+}
+
+TEST_CASE("Check insert() and erase() that they have maintain order correctly") {
+    Vector<int> v;
+
+    for (int i{0}; i < 5; ++i) 
+        v.push_back(i * 3); // 0, 3, 6, 9, 12
+    
+    v.insert(2, 30); // 0, 3, 30, 6, 9, 12
+    REQUIRE(v.size() == 6);
+    REQUIRE(v.at(2) == 30);
+    
+
+    v.erase(4); // 0, 3, 30, 6, 12
+    REQUIRE(v.size() == 5);
+    REQUIRE(v.at(4) == 12);
+
+    //Check out of bounds cases
+    int expect[] = {0, 3, 30, 6, 12};
+    REQUIRE(v.size() == 5);
+
+    REQUIRE_THROWS_AS(v.insert(12, 40), std::out_of_range);
+    REQUIRE_THROWS_AS(v.insert(-5, 8), std::out_of_range);
+
+    REQUIRE_THROWS_AS(v.erase(25), std::out_of_range);
+    REQUIRE_THROWS_AS(v.erase(-3), std::out_of_range);
+
+    REQUIRE(v.size() == 5); // stays the same
+    for (int i{0}; i < 5; ++i) //Checks that no changes were made to data
+        REQUIRE(v.at(i) == expect[i]);
+
+    REQUIRE(v.capacity() >= v.size()); //Check that capacity is still greater than size
+}
+
+TEST_CASE("Check shrink() that it correctly halves capacity when size is a fourth of the capacity") {
+    Vector<int> v;
+
+    for (int i{0}; i < 36; ++i) //Start array
+        v.push_back(i);
+    REQUIRE(v.size() == 36);
+    REQUIRE(v.capacity() >= v.size());
+
+    const int currCap = v.capacity();
+    for (int i{0}; i < 30; ++i) 
+        v.pop_back();
+    REQUIRE(v.size() == 6);   //reduce size to cause shrink to happen since size is less than 9
+    REQUIRE(v.capacity() <= currCap/2); //check that capacity is less than half the previous capacity
+    
+    //Check that it still holds the correct values in the correct order
+    for (int i{0}; i < 6; ++i) 
+        REQUIRE(v.at(i) == i);
+
+    REQUIRE(v.capacity() >= v.size()); //Check that capacity is still greater than size
+}
+
+TEST_CASE("Check shrink_to_fit that it correctly changes capacity to size (1 free slot)") {
+    Vector<int> v;
+
+    //Check empty vector returns cap of 1
+    v.shrink_to_fit();
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 1);
+
+    //Check with values
+    for (int i{0}; i < 5; ++i) 
+        v.push_back(i * 3); // 0, 3, 6, 9, 12
+
+    //use reserve to allocate space, then do shrink_to_fit and check size/capacity
+    v.reserve(50);
+    REQUIRE(v.capacity() >= 50);
+    v.shrink_to_fit();
+    REQUIRE(v.capacity() == v.size());
+
+    //Check that it still holds the correct values in the correct order
+    int expect[] = {0, 3, 6, 9, 12};
+    for (int i{0}; i < 5; ++i) 
+        REQUIRE(v.at(i) == expect[i]);
+
+    REQUIRE(v.capacity() >= v.size()); //Check that capacity is still greater than size
 }
